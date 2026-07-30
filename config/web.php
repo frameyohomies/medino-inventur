95eab6025cdd1cd55ef38db000e1750888553682
Clemens Koechl <clemens3580@gmail.com>
2026-07-30 09:16:29 +0200

    Modified web.php

--- config/web.php
+++ config/web.php
@@ -70,7 +70,7 @@ if (YII_ENV_DEV) {
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
-        'class' => \yii\gii\Module::class,
+        'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
